b6e29854ae9cc7207b28320bd23cf49ed3830023
Cat Thu Nguyen Huu <catthu@fluffy-labradoodle.lan>
2017-03-31 03:08:57 -0400

    debug

--- terminal/consumers.py
+++ terminal/consumers.py
@@ -3,10 +3,6 @@ from channels.handler import AsgiHandler
 from channels import Group
 from channels.sessions import channel_session
 
-def ws_connect(message):
-    # ASGI WebSocket packet-received and send-packet message types
-    # both have a "text" key for their textual data.
-
 def ws_receive(message):
     # ASGI WebSocket packet-received and send-packet message types
     # both have a "text" key for their textual data.
@@ -18,9 +14,3 @@ def ws_receive(message):
         "text": message.content['text'],
     })'''
 
-def ws_disconnect(message):
-    # ASGI WebSocket packet-received and send-packet message types
-    # both have a "text" key for their textual data.
-    message.reply_channel.send({
-        "text": message.content['text'],
-    })
