8693573920b9b1a4ef3654f564d5a4636696b645
Cat Thu Nguyen Huu <catthu@fluffy-labradoodle.lan>
2017-03-31 03:11:58 -0400

    all game channel

--- terminal/consumers.py
+++ terminal/consumers.py
@@ -3,10 +3,13 @@ from channels.handler import AsgiHandler
 from channels import Group
 from channels.sessions import channel_session
 
+def ws_connect(message):
+    Group('all-game').add(message.reply_channel)
+
 def ws_receive(message):
     # ASGI WebSocket packet-received and send-packet message types
     # both have a "text" key for their textual data.
-    Group('chat').send({'text': message.content['text'],})
+    Group('all-game').send({'text': message.content['text'],})
 
 
     '''
--- terminal/routing.py
+++ terminal/routing.py
@@ -2,5 +2,6 @@ from channels.routing import route
 from terminal.consumers import *
 
 channel_routing = [
+    route("websocket.connect", ws_connect),
     route("websocket.receive", ws_receive),
 ]
