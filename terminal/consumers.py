54b8df5340a9f97f8bd0bf8f241b763e8aebf3e0
Cat Thu Nguyen Huu <catthu@fluffy-labradoodle.lan>
2017-03-31 03:06:01 -0400

    debug

--- terminal/consumers.py
+++ terminal/consumers.py
@@ -6,9 +6,6 @@ from channels.sessions import channel_session
 def ws_connect(message):
     # ASGI WebSocket packet-received and send-packet message types
     # both have a "text" key for their textual data.
-    message.reply_channel.send({
-        "text": message.content['text'],
-    })
 
 def ws_receive(message):
     # ASGI WebSocket packet-received and send-packet message types
