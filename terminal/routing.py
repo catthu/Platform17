223346096eeb4183449060417ff7970bb1940ac3
Cat Thu Nguyen Huu <catthu@fluffy-labradoodle.lan>
2017-03-31 03:07:37 -0400

    debug

--- terminal/routing.py
+++ terminal/routing.py
@@ -2,6 +2,5 @@ from channels.routing import route
 from terminal.consumers import *
 
 channel_routing = [
-    route("websocket.connect", ws_connect),
     route("websocket.receive", ws_receive),
 ]
