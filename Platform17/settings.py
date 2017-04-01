7715d1735eab9f8e126cf717de61f203c2f4a39d
Cat Thu Nguyen Huu <catthu@fluffy-labradoodle.lan>
2017-04-01 03:17:40 -0400

    debug

--- Platform17/settings.py
+++ Platform17/settings.py
@@ -24,7 +24,7 @@ PROJECT_ROOT = dirname(abspath(__file__))
 SECRET_KEY = "$xke49y)rr=7%xvuis%*^q69bjll4h8m4!1ql4i_18(3ja(e#("
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # Application definition
 
@@ -159,9 +159,9 @@ CHANNEL_LAYERS = {
     },
 }
 
-'''
+
 try:
     from local_settings import *
 except ImportError as e:
     pass
-'''
+
